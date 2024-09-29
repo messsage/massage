641d15292a6da0fa0dfa708ff309ebcd5faa221c
messsage <message@041020.xyz>
2024-09-29 15:46:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,6 @@
     measurementId: "G-2K9HBCHMJW"
   };
 
-// Firebase 配置
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
 
 // 初始化 Firebase
 firebase.initializeApp(firebaseConfig);
